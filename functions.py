1b1dce8a5b265d6b5302f561a99a635f89d3fcf4
Sami <sami.ojala97@gmail.com>
2022-06-28 00:06:41 +0300

    Update

---
+++ functions.py
@@ -0,0 +1,22 @@
+import yfinance as yf
+import pandas as pd
+
+
+
+#??
+def make_a_csv():
+    pass
+
+
+#makes an excel file to store companies with their current and fair values for better visualization?
+#opens a folder for these files and stores them wiht datestamps
+def make_an_excel_file():
+    pass
+
+
+#A DCF valuation for mature dividend paying companies with 10% error marginal
+#Consider country codes for different perpetual growthrates
+def dcf_fair_value_estimation():
+    pass
+
+def draw
--- main.py
+++ main.py
@@ -1,6 +1,42 @@
+import sys
 import yfinance as yf
 import pandas as pd
+from bs4 import BeautifulSoup
+import requests
+
+
+
+#Function to extract stock data from yahoofinance.com
+#Takes a stocks ticker as a parameter
+#How to take account changing years on page??
+def extract_stock_info():
+    pass
+
+
+
+#Transform scrabed data
+#Takes data as parameter and
+def transform_data():
+    pass
+
+
+#Driver function with cmd-line based gui
+def commandline_gui():
+
+    print('Welcome to MyStockTool')
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+commandline_gui()
